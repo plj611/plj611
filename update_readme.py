80a9f41ab28aa296ac8492ce38392853e5ad8b2e
PLaw <plj611@outlook.com>
2021-10-02 17:42:59 +0800

    Code update_readme.py: add update datetime

--- update_readme.py
+++ update_readme.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 def get_activities():
     '''
@@ -14,13 +15,25 @@ def get_activities():
     acts = '\n'.join(arr)
     return acts
     
+def get_datetimenow():
+    now = datetime.utcnow()
+    return f"This README was last updated at {now.strftime('%m/%d/%Y %H:%M')} UTC by Github Actions"
+
 def update_me(section, me, contents):
     reexp = f"(?P<Part1><!-- {section} start -->\n)(?P<Part2>[\s\S]+)(?P<Part3><!-- {section} end -->)"
     return re.sub(reexp, f"\g<Part1>{contents}\g<Part3>", me)
 
 if __name__ == '__main__':
-    acts = get_activities()
     me = ' '.join(open('README.md').readlines())
+
+    acts = get_activities()
+    newme = update_me('Activities', me ,acts)
+    me = newme
+
+    dt = get_datetimenow()
+    newme = update_me('Updatetime', me, dt)
+    me = newme
+
     fd_me = open('README.md', 'w')
     fd_me.write(update_me('Activities', me, acts))
     fd_me.close()
